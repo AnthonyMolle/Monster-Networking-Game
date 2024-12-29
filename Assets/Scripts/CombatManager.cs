f68b5df8a939d47536c52f5da3df004468857554
AnthonyMolle <amolle445@gmail.com>
2024-12-28 21:29:50 -0800

    made combat camera transitions work

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CombatManager : MonoBehaviour
 {
     [SerializeField] NPC attachedNPC;
     [SerializeField] GameObject cardDisplayObject;
-
-    //TEMP FOR TESTING
-    [SerializeField] GameObject uiCombat;
+    [SerializeField] GameObject combatCam;
+    GameObject preCombatCam;
 
     public void StartCombat()
     {
-        //TEMP FOR TESTING
-        uiCombat.SetActive(true);
+        preCombatCam = CinemachineCore.Instance.GetActiveBrain(0).ActiveVirtualCamera.VirtualCameraGameObject;
+        preCombatCam.SetActive(false);
+        combatCam.SetActive(true);
         //FindObjectOfType<PlayerController>().DeactivatePlayer();
+    }
+
+    private void Update()
+    {
+        // TESTING PURPOSES
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Win();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Lose();
+        }
+        // TESTING PURPOSES
     }
 
     public void Win()
@@ -39,8 +56,9 @@ public class CombatManager : MonoBehaviour
             attachedNPC.SendDialogue();
         }
 
-        //TEMP TEST
-        uiCombat.SetActive(false);
+        combatCam.SetActive(false);
+        preCombatCam.SetActive(true);
+        preCombatCam = null;
         //FindObjectOfType<PlayerController>().ReactivatePlayer();
     }
 }
